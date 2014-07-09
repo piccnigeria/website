03609413cbdb2a501b3980dd1a22b0fce28d7b47
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-07-09 18:20:47 +0100

    Now using autoCRUD

--- app/controller.php
+++ app/controller.php
@@ -2,7 +2,14 @@
 
 class Controller extends \Lean\Controller {
 
+  # Enable magical execution of CRUD methods for RESTful resources
+  public $autoCRUD = true;
+
   protected $user = array();
+
+  protected $resourceMappings = array(
+    'Case' => 'CourtCase'
+  );
 
   protected function init(){
     $this->user = Session::get('user');
@@ -15,11 +22,8 @@ class Controller extends \Lean\Controller {
   }
 
 
-  /* Session Controllers
-   * createSession
-   * @path POST /sessions
-   * @params $email & $password
-   * @return new Session || Session::validationError
+  /**
+   * CRUD: Sessions  
    */
   public function createSession(){
     $model = Session::create($this->data);
@@ -27,13 +31,7 @@ class Controller extends \Lean\Controller {
       return $this->err = $model->getValidationError();
     return $this->responseData = Session::get();
   }
-
-  /* Session Controllers
-   * getSession
-   * @path POST /sessions
-   * @params $id
-   * @return Session::instance || error
-   */
+  
   public function getSession(){
     if ($this->params['id'] != Session::get('token')) {
       Session::invalidate();
@@ -46,14 +44,17 @@ class Controller extends \Lean\Controller {
     Session::invalidate();
   }
 
-  public function getUser(){
+  /**
+   * CRUD: Users (PICC Admins/CSOs)
+   */
+
+  /*public function getUser(){
     $model = User::get($this->params['id']);
     if (!$model)
       return $this->err = "No such user";
     return $this->responseData = $model->attrs();
   }
 
-  // Probably an admin route
   public function getUsers(){
     return $this->responseData = User::all();
   }
@@ -64,11 +65,10 @@ class Controller extends \Lean\Controller {
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
   }
+  */
 
   public function verifyUser(){
     $model = User::get($this->params['id']);
-    if (!$model)
-      return $this->err = "No such user";
     if (!$model->verify($this->data))
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
@@ -76,13 +76,12 @@ class Controller extends \Lean\Controller {
 
   public function sendCode(){
     $model = User::get($this->params['id']);
-    if (!$model)
-      return $this->err = "No such user";
     if (!$model->resendSignupMail())
       return $this->err = $model->getValidationError();
     return $this->responseData = array('success'=>"Confirmation email has been sent to {$model->email}");
   }
 
+  /*
   public function updateUser(){
     $model = User::get($this->params['id']);
     if (!$model)
@@ -93,11 +92,10 @@ class Controller extends \Lean\Controller {
     Session::set($model);
     return $this->responseData = $model->attrs();
   }
+  */
 
   protected function updatePassword(){
     $model = User::get($this->params['id']);
-    if (!$model)
-      return $this->err = "No such user";
     if (!$model->updatePassword($this->data))
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
@@ -111,28 +109,38 @@ class Controller extends \Lean\Controller {
     return $this->responseData = $model->attrs();
   }
 
+  /*
   public function destroyUser(){
     $model = User::get($this->params['id']);
     if (!$model)
       return $this->err = "No such user";
     $model->destroy();
   }
+  */
 
+  /**
+   * Create Feedback (Processes the contact us form on the website)
+   */
+  /*
   public function createFeedback(){
     $model = Feedback::create($this->data);
     if (!$model->isValid())
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
   }
+  */
 
+  /**
+   * CRUD: Subscribers
+   */
+  /*
   public function createSubscriber(){
     $model = Subscriber::create($this->data);
     if (!$model->isValid())
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
   }
-
-  // Probably and admin route
+  
   public function getSubscribers(){
     return $this->responseData = Subscriber::all();
   }
@@ -159,12 +167,258 @@ class Controller extends \Lean\Controller {
       return $this->err = "No such subscriber";
     $model->destroy();
   }
+  */
 
+  /**
+   * Create Message (sends out emails from Admin Console)
+   */  
+  /*
   public function createMessage(){
     $model = Mail::create($this->data);
     if (!$model->isValid())
       return $this->err = $model->getValidationError();
     return $this->responseData = $model->attrs();
   }
+  */
+
+  /**
+   * CRUD: Agencies
+   */
+  /*
+  public function createAgency(){
+    $model = Agency::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+  // wont work, for now. needs an inflection logic in the router
+  public function getAgencies(){
+    return $this->responseData = Agency::all();
+  }
+
+  public function getAgency(){
+    $model = Agency::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such agency";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateAgency(){
+    $model = Agency::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such agency";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyAgency(){
+    $model = Agency::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such agency";
+    $model->destroy();
+  }
+  */
+
+  /**
+   * CRUD: Cases
+   */
+
+  /*
+  public function createCase(){
+    $model = CourtCase::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+  
+  public function getCases(){
+    return $this->responseData = Case::all();
+  }
+
+  public function getCase(){
+    $model = Case::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such case";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateCase(){
+    $model = Case::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such case";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyCase(){
+    $model = Case::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such case";
+    $model->destroy();
+  }
+  */
+
+  /**
+   * CRUD: Offenders
+   */
+  /*
+  public function createOffender(){
+    $model = Offender::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function getOffenders(){
+    return $this->responseData = Offender::all();
+  }
+
+  public function getOffender(){
+    $model = Offender::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such offender";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateOffender(){
+    $model = Offender::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such offender";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyOffender(){
+    $model = Offender::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such offender";
+    $model->destroy();
+  }
+  */
+
+  /**
+   * CRUD: Courts
+   */
+
+  /*
+  public function createCourt(){
+    $model = Court::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+  
+  public function getCourts(){
+    return $this->responseData = Court::all();
+  }
+
+  public function getCourt(){
+    $model = Court::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such court";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateCourt(){
+    $model = Court::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such court";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyCourt(){
+    $model = Court::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such court";
+    $model->destroy();
+  }
+  */
+
+  /**
+   * CRUD: Judges
+   */
+  /*
+  public function createJudge(){
+    $model = Judge::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+  
+  public function getJudges(){
+    return $this->responseData = Judge::all();
+  }
+
+  public function getJudge(){
+    $model = Judge::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such judge";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateJudge(){
+    $model = Judge::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such judge";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyJudge(){
+    $model = Judge::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such judge";
+    $model->destroy();
+  }
+  */
+
+  /**
+   * CRUD: Trials
+   */
+  /*
+  public function createTrial(){
+    $model = Trial::create($this->data);
+    if (!$model->isValid())
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function getTrials(){
+    return $this->responseData = Trial::all();
+  }
+
+  public function getTrial(){
+    $model = Trial::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such trial";
+    return $this->responseData = $model->attrs();
+  }
+
+  public function updateTrial(){
+    $model = Trial::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such trial";
+    if (!$model->save( $this->data ))
+      return $this->err = $model->getValidationError();
+    return $this->responseData = $model->attrs();
+  }
+
+  public function destroyTrial(){
+    $model = Trial::get($this->params['id']);
+    if (!$model)
+      return $this->err = "No such trial";
+    $model->destroy();
+  }
+  */
+
+
+  
+
 
 }
