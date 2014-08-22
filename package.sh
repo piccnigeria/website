4baf64f98b822628fde916c3d329f6a9bb6cb971
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-08-22 02:55:15 +0100

    Updated About Us page; added terms and conditions

--- package.sh
+++ package.sh
@@ -54,7 +54,7 @@ echo "App updated successfully!"
 echo "Adding changes to git..."
 git add .
 echo "Committing changes to git..."
-git commit -m "Updated About us content"
+git commit -m "Updated About Us page; added terms and conditions"
 echo "Pushing changes to Github - git@github.com:piccnigeria/website.git"
 git push origin master
 echo "Pushing changes to Heroku - git@heroku.com:piccnigeria.git"
