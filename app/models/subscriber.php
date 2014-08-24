74a97d2359b9b76c8b5d65a6e1ebd1e7b25dfc46
Temiyemi Agbetunsin <temi.yemi@gmail.com>
2014-08-24 17:16:01 +0100

    Dropped name requirement for subscription

--- app/models/subscriber.php
+++ app/models/subscriber.php
@@ -2,21 +2,21 @@
 
 class Subscriber extends \Lean\Model\Base {
 
-	protected $order = 'name asc';
+	protected $order = 'email asc';
 
 	protected $validations = array(
-  	  'create' => array(
-  	    'presence' => 'name email',
-  	    'email' => 'email',
-  	    'unique' => 'email'
-  	  ),
-  	  'update' => array(
-  	    'not_null' => 'name email'
-  	  )
-  	);
+	  'create' => array(
+	    'presence' => 'email',
+	    'email' => 'email',
+	    'unique' => 'email'
+	  ),
+	  'update' => array(
+	    'not_null' => 'email'
+	  )
+	);
 
   protected function beforeCreate(array &$attrs) {
-    $attrs ['name'] = ucwords( strtolower( $attrs ['name'] ) );
+    # $attrs ['name'] = ucwords( strtolower( $attrs ['name'] ) );
     $attrs ['email'] = strtolower( $attrs ['email'] );
   }
 		
